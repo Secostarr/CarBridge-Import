28f3fc4d76c0a042d59838c4e0714e8135ccbd0b
Secostarr <bintangdika676@gmail.com>
2024-12-16 14:14:37 +0700

    memperbaiki tata letak gambar pada home utama

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+
+Route::get('/carbridge', [HomeController::class, 'home'])->name('home ');
