33b01f558838e8531ccb37af93c892b6706bf34f
FarrosAufa1 <farosaufa53@gmail.com>
2024-12-17 12:17:10 +0700

    membuat dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/carbridge', [HomeController::class, 'home'])->name('home');
 Route::get('/carbridge/item/seeAll', [HomeController::class, 'seeAll'])->name('seeAll');
+Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
